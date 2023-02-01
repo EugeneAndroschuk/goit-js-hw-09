b5337c1d46f502e9640ff0f9df7aaebab5b43bc8
EugeneAndroschuk <enet2611@gmail.com>
2023-02-01 19:45:40 +0200

    switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-stop]');
+let intervalId = 0;
 
+startBtn.addEventListener('click', onChangeBgdColor);
+stoptBtn.addEventListener('click', onStopChangeBgdColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onChangeBgdColor() {
+  intervalId = setInterval(() => {
+    const newBgdColor = getRandomHexColor();
+
+    document.body.style.backgroundColor = newBgdColor;
+  }, 1000);
+
+  startBtn.disabled = true;
+}
+
+function onStopChangeBgdColor() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
