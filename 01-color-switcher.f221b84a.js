4fc43f49de2470397d2e8d71df0182399ce50de7
EugeneAndroschuk <111381321+EugeneAndroschuk@users.noreply.github.com>
2023-02-01 17:45:52 +0000

    Deploying to gh-pages from @ EugeneAndroschuk/goit-js-hw-09@b5337c1d46f502e9640ff0f9df7aaebab5b43bc8 🚀

---
+++ 01-color-switcher.f221b84a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=0;t.addEventListener("click",(function(){n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.f221b84a.js.map
