33073b75de3e11010b227b3fc41033765b5e546b
EugeneAndroschuk <enet2611@gmail.com>
2023-02-03 23:36:55 +0200

    timer and promices done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,7 @@
 import flatpickr from 'flatpickr';
-// Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const inputDate = document.getElementById('datetime-picker');
 const spanDays = document.querySelector('span[data-days]');
 const spanHours = document.querySelector('span[data-hours]');
 const spanMinutes = document.querySelector('span[data-minutes]');
@@ -14,35 +13,29 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+  onClose: date => {
+    selectDate = date[0].getTime();
+    if (selectDate < Date.now()) {
+      Notify.failure('Please choose a date in the future', {position: "center-top",});
+      startBtn.disabled = true;
+    } else startBtn.disabled = false;
   },
 };
 
+startBtn.disabled = true;
 startBtn.addEventListener('click', onCountDown);
 
 flatpickr('#datetime-picker', options);
-flatpickr('#datetime-picker', {
-  onClose: date => {
-    selectDate = date[0].getTime();
-    const selectedDate = date[0].getTime();
-  },
-});
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -50,15 +43,17 @@ function convertMs(ms) {
 
 function onCountDown() {
   setInterval(() => {
-      const currentDate = Date.now();
-      const diff = selectDate - currentDate;
+    const currentDate = Date.now();
+    const diff = selectDate - currentDate;
+    if (diff > 0) {
       spanDays.textContent = addLeadingZero(convertMs(diff).days);
       spanHours.textContent = addLeadingZero(convertMs(diff).hours);
       spanMinutes.textContent = addLeadingZero(convertMs(diff).minutes);
       spanSeconds.textContent = addLeadingZero(convertMs(diff).seconds);
+    }
   }, 1000);
 }
 
 function addLeadingZero(value) {
-    return String(value).padStart(2, 0);
+  return String(value).padStart(2, 0);
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onClick);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) resolve({ position, delay });
+      else reject({ position, delay });
+    }, delay);
+  });
+}
+
+function onClick(e) {
+  e.preventDefault();
+  let delayTimeout = +delay.value;
+  const stepTimeout = +step.value;
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delayTimeout)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`, {timeout: 10000,});
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`, {timeout: 10000,});
+      });
+    delayTimeout += stepTimeout;
   }
 }
